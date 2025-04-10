9f10e3d5cb6c98bba5d5f8141607642143fd766b
haliwt <jonewang@126.com>
2025-04-10 16:39:25 +0800

    update compile to pass.DATA.2025.04.10

--- hardware/src/sensor.c
+++ hardware/src/sensor.c
@@ -46,19 +46,15 @@ void Do_Charge(void)
 		TRISAbits.TRISA0 = 1 ; //as input  GPIO --
         if(Charging_VoltageValue()  < 150  ){ //charing 0.9V =184
 		     cmd_t.gCmd = TempStop;
-             cmd_t.gDoKey =1;
+           
             if(dochargingFlag ==0){
                 dochargingFlag++;
-		     if(cmd_t.gCmd_KeyNum == 1)cmd_t.gCmd_KeyNum =0;
-			 else if(cmd_t.gCmd_KeyNum == 3)cmd_t.gCmd_KeyNum =2;
+		    
             }
 			
 			 
 		}
-        else{
-             cmd_t.gDoKey =0;
-            
-         }
+       
 	}
   
 }
